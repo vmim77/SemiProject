b69f725a58b1cdb50a5a4a996438240521a1f7b5
superjaeseong <jslee4176@naver.com>
2021-10-01 16:51:30 +0900

    재성 테스트 돈 터치!!!!!!!!!!!!!!2222

---
+++ SemiProject/src/jaeseongtest2222/Jaeseongtestdonttouch22222.java
@@ -0,0 +1,10 @@
+package jaeseongtest2222;
+
+public class Jaeseongtestdonttouch22222 {
+
+	public static void main(String[] args) {
+		System.out.println(" 재성 테스트 돈 터치!!!!!!!!!!!!!!2222");
+
+	}
+
+}
